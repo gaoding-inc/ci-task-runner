a998b18072cf4580270e99b46826a59d75ebf897
糖饼 <1987.tangbin@gmail.com>
2016-11-14 11:51:39 +0800

    修改文档

--- test/parallel/index.js
+++ test/parallel/index.js
@@ -1,49 +1,49 @@
 const child_process = require('child_process');
 
-console.time('parallel:1');
+console.time('「「parallel:1」」');
 console.log(child_process.execSync('git-webpack --parallel=1', {
     cwd: __dirname
 }).toString());
-console.timeEnd('parallel:1');
+console.timeEnd('「parallel:1」');
 
-console.time('parallel:2');
+console.time('「parallel:2」');
 console.log(child_process.execSync('git-webpack --parallel=2', {
     cwd: __dirname
 }).toString());
-console.timeEnd('parallel:2');
+console.timeEnd('「parallel:2」');
 
-console.time('parallel:3');
+console.time('「parallel:3」');
 console.log(child_process.execSync('git-webpack --parallel=3', {
     cwd: __dirname
 }).toString());
-console.timeEnd('parallel:3');
+console.timeEnd('「parallel:3」');
 
-console.time('parallel:4');
+console.time('「parallel:4」');
 console.log(child_process.execSync('git-webpack --parallel=4', {
     cwd: __dirname
 }).toString());
-console.timeEnd('parallel:4');
+console.timeEnd('「parallel:4」');
 
-console.time('parallel:5');
+console.time('「parallel:5」');
 console.log(child_process.execSync('git-webpack --parallel=5', {
     cwd: __dirname
 }).toString());
-console.timeEnd('parallel:5');
+console.timeEnd('「parallel:5」');
 
-console.time('parallel:6');
+console.time('「parallel:6」');
 console.log(child_process.execSync('git-webpack --parallel=6', {
     cwd: __dirname
 }).toString());
-console.timeEnd('parallel:6');
+console.timeEnd('「parallel:6」');
 
-console.time('parallel:7');
+console.time('「parallel:7」');
 console.log(child_process.execSync('git-webpack --parallel=7', {
     cwd: __dirname
 }).toString());
-console.timeEnd('parallel:7');
+console.timeEnd('「parallel:7」');
 
-console.time('parallel:8');
+console.time('「parallel:8」');
 console.log(child_process.execSync('git-webpack --parallel=8', {
     cwd: __dirname
 }).toString());
-console.timeEnd('parallel:8');
+console.timeEnd('「parallel:8」');
