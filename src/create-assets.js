f24d34f4c4479ac47e11e3b490f9339b9d9ca5f9
糖饼 <1987.tangbin@gmail.com>
2016-11-21 11:22:34 +0800

    修复 librarysCommit错误

--- src/create-assets.js
+++ src/create-assets.js
@@ -6,42 +6,20 @@ const ASSETS_DEFAULT_NAME = './config/assets.default.json';
 const ASSETS_DEFAULT = require(ASSETS_DEFAULT_NAME);
 
 
-module.exports = (assetsPath, modulesAssets, latestCommit) => {
-
-    let modulesMap = {};
-    let librarysCommit = {};
-
-    modulesAssets.forEach(mod => {
-        let name = mod.name;
-        mod.commit = latestCommit[name];
-        modulesMap[name] = mod;
-        librarysCommit[name] = latestCommit[name];
-        delete modulesMap[name].name;
-    });
-
-    let assetsContent = defaultsDeep({}, {
-        librarys: librarysCommit
-    }, ASSETS_DEFAULT);
-
-    assetsContent.date = (new Date()).toLocaleString();
-    assetsContent.modules = modulesMap;
-
-    // 保存资源索引文件（TIPS: 为了保证有效性，要第一时间读取最新描述文件再写入）
+module.exports = (assetsPath, modulesAssets, latestCommit, librarysCommit) => {
+    // 并行写配置需要第一时间读取再操作
     return readAssets(assetsPath).then(preAssetsContent => {
 
-        let modulesMap = assetsContent.modules;
+        let modulesMap = {};
+        let latest = [];
         let relative = file => path.relative(path.dirname(assetsPath), file);
-        let latest = []
 
-        Object.keys(modulesMap).forEach(name => {
-
-            latest.push(name);
-
+        modulesAssets.forEach(mod => {
+            let name = mod.name;
             let preModMap = preAssetsContent.modules[name];
-            let mod = modulesMap[name];
             let chunks = mod.chunks;
             let assets = mod.assets;
-
+            
             // 自动递增模块的编译版本号
             if (preModMap) {
                 mod.version = preModMap.version + 1;
@@ -50,18 +28,28 @@ module.exports = (assetsPath, modulesAssets, latestCommit) => {
             // 将绝对路径转换为相对与资源描述文件的路径
             Object.keys(chunks).forEach(name => chunks[name] = relative(chunks[name]));
             assets.forEach((file, index) => assets[index] = relative(file));
+            
+            mod.commit = latestCommit[name];
 
+            latest.push(name);
+            modulesMap[name] = mod;
+
+            delete modulesMap[name].name;
         });
 
-        Object.assign(preAssetsContent.modules, modulesMap);
-        Object.assign(preAssetsContent.librarys, assetsContent.librarys);
-        preAssetsContent.version = (Number(preAssetsContent.version) || 0) + 1;
-        preAssetsContent.latest = latest;
-        preAssetsContent.date = (new Date()).toLocaleString();
+        let assetsContent = defaultsDeep({
+            version: (Number(preAssetsContent.version) || 0) + 1,
+            date: (new Date()).toLocaleString(),
+            latest: latest,
+            modules: modulesMap,
+            librarys: librarysCommit
+        }, ASSETS_DEFAULT);
 
-        let newAssetsContentText = JSON.stringify(preAssetsContent, null, 2);
 
-        return fsp.writeFile(assetsPath, newAssetsContentText, 'utf8').then(() => preAssetsContent);
+        Object.assign(assetsContent.modules, preAssetsContent.modules);
+        Object.assign(assetsContent.librarys, preAssetsContent.librarys);
 
+        let data = JSON.stringify(assetsContent, null, 2);
+        return fsp.writeFile(assetsPath, data, 'utf8').then(() => assetsContent);
     })
 }
