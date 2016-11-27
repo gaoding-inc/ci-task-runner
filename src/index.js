95fb9d08c30261b4b1ca9a3677c6446eb354c669
糖饼 <1987.tangbin@gmail.com>
2016-11-27 18:55:15 +0800

    支持配置仓库类型为SVN

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 const path = require('path');
 const defaultsDeep = require('lodash.defaultsdeep');
 const promiseTask = require('../lib/promise-task');
-const GitCommit = require('../lib/git-commit');
+const Repository = require('../lib/repository');
 const Loger = require('../lib/loger');
 const DEFAULT = require('./config/config.default.json');
 
@@ -21,6 +21,7 @@ const assets = require('./assets');
  * @param   {Object}            options.modules.builder    模块构建器设置，继承 options.builder
  * @param   {string[]}          options.librarys           模块组公共依赖（相对）
  * @param   {string}            options.assets          构建后文件索引表输出路径（相对）
+ * @param   {string}            options.repository      仓库类型，可选 git|svn
  * @param   {number}            options.parallel        最大进程数
  * @param   {boolean}           options.force           是否强制全部构建
  * @param   {Object}            options.builder         构建器设置
@@ -43,7 +44,7 @@ module.exports = (options = {}, context = process.cwd()) => {
     
     const assetsPath = path.resolve(context, options.assets);
     const loger = new Loger();
-    const gitCommit = new GitCommit(assetsPath);
+    const repository = new Repository(assetsPath, options.repository, 'revision');
 
     return promiseTask.serial([
 
@@ -54,8 +55,8 @@ module.exports = (options = {}, context = process.cwd()) => {
             return Promise.all(modules.map(mod => {
                 return Promise.all([
 
-                    gitCommit.watch(mod.path),
-                    ...mod.librarys.map(lib => gitCommit.watch(lib.path))
+                    repository.watch(mod.path),
+                    ...mod.librarys.map(lib => repository.watch(lib.path))
 
                 ]).then(([modCommit, ...libCommits]) => {
 
@@ -93,10 +94,10 @@ module.exports = (options = {}, context = process.cwd()) => {
         },
 
 
-        // 保存最新的版本信息（git commid id）
+        // 保存最新的版本信息
         assetsContent => {
             // 必须构建完才保存版本信息，否则构建失败后下一次可能不会重新构建
-            return gitCommit.save().then(() => assetsContent);
+            return repository.save().then(() => assetsContent);
         }
 
     ]);
