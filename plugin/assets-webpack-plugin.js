348ec56e64327962c54309090e7c38468e391a9b
糖饼 <1987.tangbin@gmail.com>
2016-11-29 19:53:14 +0800

    临时修复linux子进程pid与child.pid不相等导致的BUG

--- plugin/assets-webpack-plugin.js
+++ plugin/assets-webpack-plugin.js
@@ -14,7 +14,7 @@ class AssetsWebpackPlugin {
 
     send(result) {
         process.nextTick(() => {
-            worker.send(result, process.pid);
+            worker.send(result);
         });
     }
 
