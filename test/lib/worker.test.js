41ef093e513d931f07dc6616d085a33e7c44087b
糖饼 <1987.tangbin@gmail.com>
2016-12-06 10:11:18 +0800

    添加测试用例

--- test/lib/worker.test.js
+++ test/lib/worker.test.js
@@ -36,4 +36,51 @@ describe('#worker', () => {
         });
     });
 
+
+    it('childProcess exec: env inherit', () => {
+        let cwd = path.join(__dirname, '..', 'file', 'script');
+        let TEST_ID = Date.now().toString();
+
+        process.env.TEST_ID = TEST_ID;
+
+        return worker(`cd ${cwd} && node worker-child.js`, {
+            env: {
+                TEST_ID
+            }
+        }).then(data => {
+            assert.deepEqual({
+                cwd,
+                id: TEST_ID
+            }, data);
+        });
+    });
+
+    it('childProcess exec: env rewrite', () => {
+        let cwd = path.join(__dirname, '..', 'file', 'script');
+        let TEST_ID = Date.now().toString();
+
+        process.env.TEST_ID = 'hello';
+
+        return worker(`cd ${cwd} && node worker-child.js`, {
+            env: {
+                TEST_ID
+            }
+        }).then(data => {
+            assert.deepEqual({
+                cwd,
+                id: TEST_ID
+            }, data);
+        });
+    });
+
+    it('childProcess exec: timeout', () => {
+        let cwd = path.join(__dirname, '..', 'file', 'script');
+
+        return worker(`cd ${cwd} && node worker-child.js`, {
+            timeout: 1
+        }).then(() => {
+            throw new Error('"timeout" does not work');
+        }, () => {});
+    });
+
 });
